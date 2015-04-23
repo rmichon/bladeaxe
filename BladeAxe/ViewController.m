36f975ae40e05f53b0d0106886d2a2bbe31ff2cb
Romain Michon <rmnmichon@gmail.com>
2015-04-22 21:27:49 -0700

    First commit

---
+++ BladeAxe/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BladeAxe
+//
+//  Created by Romain Michon on 4/22/15.
+//  Copyright (c) 2015 CCRMA. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
